77f2fb2bcd4471ef5e29240abdd7ee71f8095bd7
jaeditka <wi19b017@technikum-wien.at>
2023-10-11 15:16:13 +0200

    db connection

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,9 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,12 +11,22 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Button button1;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Exam manager");
+
+        //button1 = new Button();
+        //button1.setText("Categories");
+        //StackPane layout = new StackPane();
+        //layout.getChildren().add(button1);
+
         primaryStage.setScene(new Scene(root, 300, 275));
+
         primaryStage.show();
+
     }
 
 
